cc4a4fb3d7dc387c52c9fedc0b211f82a6fbeff7
tietor <fabio.torti@tie.ch>
2018-12-08 17:31:44 +0100

    Call Method with main method

--- src/main/java/CalculatorApp.java
+++ src/main/java/CalculatorApp.java
@@ -1,2 +1,16 @@
 public class CalculatorApp {
+
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+    int valueA = 0;
+    int valueB = 0;
+
+    System.out.println("Console Calculator");
+    System.out.println("==================");
+    System.out.println();
+
+    valueA =10;
+    valueB =20;
+    System.out.println("Summe "+valueA +" + "+valueB +" = "+calculator.summe(10,20));
+  }
 }
