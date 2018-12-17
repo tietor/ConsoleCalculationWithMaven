d87d1aadfbf4ca6b5dd093312cfb809ee5f74d1f
5ia17fatorti <5ia17fatorti@gibwintra.ch>
2018-12-17 08:13:38 +0100

    Test Calculator with first unittest

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,4 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestCalculator {
+  Calculator calculator;
 
+  @Test
+  public void testSummeZweiPositiveIsOk() {
+    calculator = new Calculator();
+    assertTrue(calculator.summe(10, 25) == 35);
+  }
 }
