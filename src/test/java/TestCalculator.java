d93d3c7a19d9d6f532769f7a427b961dee309e5c
tietor <fabio.torti@tie.ch>
2019-01-02 00:17:33 +0100

    Remove calculator instances for every testmethod

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -14,13 +14,11 @@ public class TestCalculator {
 
   @Test
   public void testSummeZweiPositiveIsOk() {
-    calculator = new Calculator();
     assertTrue(calculator.summe(10, 25) == 35);
   }
 
   @Test
   public void testSubtractionZweiPositiveIsOk() {
-    calculator = new Calculator();
     assertTrue(calculator.subtraktion(25, 10) == 15);
   }
 
